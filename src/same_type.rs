0dd21691cf854501009899aba7251bcb4a0ec20b
sirati <git.public@sirati.me>
2024-09-18 01:36:48 +0200

    add conversion helper for SameSelf

--- src/same_type.rs
+++ src/same_type.rs
@@ -1,7 +1,13 @@
 trait Sealed<T>: Sized{}
 #[allow(private_bounds)]
 pub trait Same<T1, T2>: Sealed<T1> + Sealed<T2>{}
-pub trait SameSelf<TOther>: Same<TOther, Self> + Same<Self, TOther>{}
+pub trait SameSelf<TOther>: Same<TOther, Self> + Same<Self, TOther>{
+    fn into_other_self(self) -> TOther;
+}
 impl<T: Same<T, T>> Sealed<T> for T{}
-impl<T: Same<T, T>> SameSelf<T> for T{}
+impl<T: Same<T, T>> SameSelf<T> for T{
+    fn into_other_self(self) -> T {
+        self
+    }
+}
 impl<T> Same<T, T> for T{}
