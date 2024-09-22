d18eac347e6155ef4bccc49bfdc6b59798c9ad9b
sirati <git.public@sirati.me>
2024-09-22 20:02:11 +0200

    same type must be properly sealed in order to provide the guarantees it does

--- src/same_type.rs
+++ src/same_type.rs
@@ -1,11 +1,13 @@
-trait Sealed<T>: Sized{}
+trait Sealed<T1, T2>: Sized{}
 #[allow(private_bounds)]
-pub trait Same<T1, T2>: Sealed<T1> + Sealed<T2>{}
+pub trait Same<T1, T2>: Sealed<T1, T2> + Sealed<T2, T1>{}
 pub trait SameSelf<TOther>: Same<TOther, Self> + Same<Self, TOther>{
     fn into_other_self(self) -> TOther;
 }
-impl<T: Same<T, T>> Sealed<T> for T{}
+impl<T: Same<T, T>> Sealed<T, T> for T{}
 impl<T: Same<T, T>> SameSelf<T> for T{
+    // this is a no-op, so we must avoid the move
+    #[inline(always)]
     fn into_other_self(self) -> T {
         self
     }
